3bf4e0714af722520b789fff0b85d0513a1e391d
Ankit Kumar Mourya <ankitkumarmourya1@gmail.com>
2022-12-10 19:16:30 +0530

    added a comment in division

--- div.cpp
+++ div.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main(){
     float a=5,b=2;
-    cout<<"Division of the number is :" <<(a/b);
+    cout<<"Division of the number is :" <<(a/b)<<endl;
+    cout<<"division done";
 }  
